951165021912f75d0ddffe531aa8a1f189f39f79
Sheldon Thomas <sheldon.thomas@me.com>
2015-05-16 16:07:48 -0400

    Remove Log Statements

--- index.js
+++ index.js
@@ -34,12 +34,10 @@ module.exports = function (opt) {
     }, opt);
 
     try {
-      console.log(str);
       data = iced.compile(str, options);
     } catch (err) {
       return cb(new PluginError('gulp-iced-coffee', err));
     }
-    console.log(data);
 
     if (data && data.v3SourceMap && file.sourceMap) {
       applySourceMap(file, data.v3SourceMap);
